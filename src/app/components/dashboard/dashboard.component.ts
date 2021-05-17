2140300b6460cd441cd3c2ee00cda22aaf928dba
Zsolt Csáky <zsolt@csaky.hu>
2021-05-17 17:20:08 +0200

    Rig sorting added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { PriceData } from 'src/app/models/price-data';
-import { CreateRigWithHelpers, RigWithHelpers } from 'src/app/models/rig';
+import { CreateRigWithHelpers, Rig, RigWithHelpers } from 'src/app/models/rig';
+import { RigStatus } from 'src/app/models/rig-snapshot';
 import { PriceService } from 'src/app/services/price/price.service';
 import { RigService } from 'src/app/services/rig/rig.service';
+
+const PriorityByMiningStatus: Map<RigStatus, number> = new Map([
+  [ RigStatus.MINING, 0 ],
+  [ RigStatus.BENCHMARKING, 1 ],
+  [ RigStatus.ERROR, 2 ],
+  [ RigStatus.PENDING, 3 ],
+  [ RigStatus.TRANSFERRED, 4 ],
+  [ RigStatus.STOPPED, 5 ],
+  [ RigStatus.DISABLED, 6 ],
+  [ RigStatus.OFFLINE, 7 ],
+  [ RigStatus.UNKNOWN, 8 ]
+]);
 
 @Component({
   selector: 'app-dashboard',
@@ -20,9 +34,22 @@ export class DashboardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.rigService.getRigSnapshotsCombined().subscribe(rigs => {
-      this.rigs = rigs.map(rig => CreateRigWithHelpers(rig));
+    this.rigService.getRigSnapshotsCombined().pipe(
+      map((rigs: Rig[]) => rigs.map(rig => CreateRigWithHelpers(rig))),
+      map(rigs => this.sortRigs(rigs))
+    ).subscribe(rigs => {
+      this.rigs = rigs;
     });
     this.priceService.getPriceSource().subscribe(priceData => this.price = priceData);
   }
+
+  // Private helpers
+
+  private sortRigs(rigs: RigWithHelpers[]): RigWithHelpers[] {
+    return rigs.sort((a, b) =>
+      PriorityByMiningStatus.get(a.latestSnapshot.minerStatus) - PriorityByMiningStatus.get(b.latestSnapshot.minerStatus)
+      || a.name.localeCompare(b.name)
+    );
+  }
+
 }
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
 @NgModule({
   exports: [
     MatSidenavModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule
   ],
 })
 export class MaterialModule {}
